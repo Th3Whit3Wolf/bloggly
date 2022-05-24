9a45b3e1ff17b7aeb949c57e5ee7c9cada38dc9d
David Karrick <the.white.wolf.is.1337@gmail.com>
2022-05-23 22:39:07 -0600

    Chore: Setup mocks for server

--- ui/mocks/server.js
+++ ui/mocks/server.js
@@ -1,7 +1,6 @@
-// src/mocks/browser.js
-import { setupWorker } from "msw";
+// from https://github.com/mswjs/examples/blob/master/examples/with-jest/src/mocks/server.js
+import { setupServer } from "msw/node";
 
 import { handlers } from "./handlers";
 
-// This configures a Service Worker with the given request handlers.
-export const worker = setupWorker(...handlers);
+export const server = setupServer(...handlers);
