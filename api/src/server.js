a1dd48623e8527ff6f2ca4affa4e2b6ec92b9017
David Karrick <the.white.wolf.is.1337@gmail.com>
2022-05-23 22:22:40 -0600

    Fix: polite redirect

--- api/src/server.js
+++ api/src/server.js
@@ -42,7 +42,7 @@ app.use(log);
 if (NODE_ENV === "production") {
 	app.use(
 		cors({
-			origin: "https://bsdi1-voyager-frontend.herokuapp.com",
+			origin: "https://bloggly-ui.herokuapp.com/",
 			allowedHeaders:
 				"Origin, X-Requested-With, Content-Type, Accept, Authorization",
 			methods: ["GET", "PUT", "POST", "DELETE"]
