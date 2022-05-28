547df2d6d090116258b3ae0ed7a45b36a738d079
David Karrick <the.white.wolf.is.1337@gmail.com>
2022-05-28 16:38:31 -0600

    Chore: Tweak landing page layout

--- ui/src/Pages/Landing.jsx
+++ ui/src/Pages/Landing.jsx
@@ -1,14 +1,6 @@
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 
-import {
-	Box,
-	Grid,
-	Typography,
-	useTheme,
-	Button,
-	Container
-} from "@mui/material";
+import { Box, Grid, Typography, useTheme } from "@mui/material";
 import {
 	EngineeringOutlined as EngineeringOutlinedIcon,
 	AllInclusive as AllInclusiveIcon,
@@ -18,7 +10,6 @@ import HeroIMG from "../Assets/Imgs/hero.jpg";
 
 const Landing = () => {
 	const theme = useTheme();
-	const navigate = useNavigate();
 	const sectionItems = [
 		{
 			id: 1,
@@ -58,15 +49,6 @@ const Landing = () => {
 		}
 	];
 
-	const handleLogin = e => {
-		e.preventDefault();
-		navigate("/login");
-	};
-
-	const handleSignUp = e => {
-		e.preventDefault();
-		navigate("/signup");
-	};
 	useEffect(() => {
 		document.title = "Welcome to Bloggly ";
 	}, []);
@@ -74,23 +56,26 @@ const Landing = () => {
 	return (
 		<Box
 			sx={{
-				p: 0,
-				mt: "100px",
-				width: "100%",
-				height: "calc(80vh - 100px)",
+				position: "absolute",
+				top: "100px",
+				left: 0,
+				width: "100vw",
+				height: "77vh",
 				backgroundImage: `url(${HeroIMG})`,
 				backgroundPosition: "center",
 				backgroundRepeat: "no-repeat",
-				backgroundSize: "cover"
+				backgroundSize: "cover",
+				p: 0,
+				m: 0
 			}}
 		>
-			<Grid container spacing={2}>
-				<Grid item xs={12} sx={{ mb: "2rem" }}>
+			<Grid container spacing={2} sx={{ mt: 0, pt: 0 }}>
+				<Grid item xs={12} sx={{ height: "77vh", pt: 0, mt: 0 }}>
 					<Typography
 						variant="h3"
 						sx={{
 							textAlign: "center",
-							mt: "calc(100px + 10vh)",
+							mt: "10vh",
 							color: "#fff"
 						}}
 					>
@@ -101,44 +86,13 @@ const Landing = () => {
 						sx={{
 							textAlign: "center",
 							mt: "1vh",
-							mb: "39vh",
+							mb: "calc(77vh - 11vh - 8.6876rem - 16px)",
 							color: "#eee"
 						}}
 					>
 						Blogs and stuff
 					</Typography>
-				</Grid>
-				<Grid item xs={2} sx={{ mb: "2rem" }}>
-					<Container
-						disableGutters
-						sx={{
-							display: "flex",
-							flexDirection: "row",
-							justifyContent: "flex-end",
-							color: theme.palette.gsb.text,
-							alignItems: "left",
-							whiteSpace: "no-wrap",
-							pr: "1rem"
-						}}
-					>
-						<Button
-							variant="contained"
-							onClick={handleLogin}
-							data-testid="loginButtonHero"
-							sx={{
-								mt: "0.25rem",
-								ml: "0.5rem",
-								borderWidth: "2px",
-								borderRadius: "12px",
-								backgroundColor: theme.palette.gsb.primary,
-								flexShrink: 0
-							}}
-						>
-							Log In
-						</Button>
-					</Container>
-				</Grid>
-				<Grid item xs={8} sx={{ mb: "2rem" }}>
+
 					<Typography
 						variant="h6"
 						sx={{
@@ -149,38 +103,8 @@ const Landing = () => {
 						come for the blogs and stay for the stuff
 					</Typography>
 				</Grid>
-				<Grid item xs={2} sx={{ mb: "2rem" }}>
-					<Container
-						disableGutters
-						sx={{
-							display: "flex",
-							flexDirection: "row",
-							justifyContent: "flex-start",
-							color: theme.palette.gsb.text,
-							alignItems: "left",
-							whiteSpace: "no-wrap",
-							pr: "1rem"
-						}}
-					>
-						<Button
-							variant="Sign Up"
-							onClick={handleSignUp}
-							data-testid="signupButtonHero"
-							sx={{
-								ml: "0.5rem",
-								color: theme.palette.gsb.primary,
-								borderWidth: "2px",
-								borderStyle: "solid",
-								borderRadius: "12px",
-								borderColor: theme.palette.gsb.primary,
-								flexShrink: 0
-							}}
-						>
-							Sign Up
-						</Button>
-					</Container>
-				</Grid>
-				<Grid item xs={12}>
+
+				<Grid item xs={12} sx={{ mt: "1rem" }}>
 					<Grid
 						container
 						spacing={2}
@@ -199,7 +123,6 @@ const Landing = () => {
 								sx={{
 									p: "0.25rem 0.5rem",
 									m: "1rem",
-									mt: "2rem",
 									textAlign: "center",
 									backgroundColor: theme.palette.gsb.background,
 									borderRadius: "10px",
