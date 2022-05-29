970393dd1952f67af4c967f5fe416eebcac5432a
David Karrick <the.white.wolf.is.1337@gmail.com>
2022-05-29 03:11:25 -0600

    Style: Landing scale better

--- ui/src/Pages/Landing.jsx
+++ ui/src/Pages/Landing.jsx
@@ -1,5 +1,12 @@
 import { useEffect, forwardRef } from "react";
-import { Box, Grid, Typography, useTheme, Button } from "@mui/material";
+import {
+	Box,
+	Grid,
+	Typography,
+	useTheme,
+	Button,
+	Container
+} from "@mui/material";
 import {
 	EngineeringOutlined as EngineeringOutlinedIcon,
 	AllInclusive as AllInclusiveIcon,
@@ -9,6 +16,7 @@ import {
 import { NavLink } from "react-router-dom";
 
 import HeroIMG from "../Assets/Imgs/hero.jpg";
+import { MainContainer } from "#Components";
 
 const LinkBehavior = forwardRef((props, ref) => (
 	<NavLink ref={ref} to="/" {...props} role={undefined} />
@@ -75,102 +83,103 @@ const Landing = () => {
 				m: 0
 			}}
 		>
-			<Grid container spacing={2} sx={{ mt: 0, pt: 0 }}>
-				<Grid item xs={12} sx={{ height: "77vh", pt: 0, mt: 0 }}>
-					<Typography
-						variant="h3"
-						sx={{
-							textAlign: "center",
-							mt: "10vh",
-							color: "#fff"
-						}}
-					>
-						Bloggly
-					</Typography>
-					<Typography
-						variant="h4"
-						sx={{
-							textAlign: "center",
-							mt: "1vh",
-							mb: "calc(77vh - 14vh - 8.6876rem - 16px)",
-							color: "#eee"
-						}}
-					>
-						Blogs and stuff
-					</Typography>
-
-					<Typography
-						variant="h6"
-						sx={{
-							textAlign: "center",
-							color: "#333"
-						}}
-					>
-						come for the blogs and stay for the stuff
-					</Typography>
-					<Typography
-						component="div"
-						sx={{
-							textAlign: "center"
-						}}
-					>
-						<Button
-							variant="contained"
-							aria-label="view posts"
-							data-testid="viewAllPostsButton"
-							component={LinkBehavior}
-							to="/posts"
-							startIcon={<MessageIcon />}
+			<Container sx={{ pl: "4rem", pr: "4rem", maxWidth: "125ch" }}>
+				<Grid container spacing={2} sx={{ mt: 0, pt: 0 }}>
+					<Grid item xs={12} sx={{ height: "77vh", pt: 0, mt: 0 }}>
+						<Typography
+							variant="h3"
 							sx={{
-								mt: "0.25rem",
-								ml: "0.75rem",
-								p: "6px 16px",
-								borderRadius: "12px",
-								borderColor: theme.palette.gsb.primary,
-								backgroundColor: theme.palette.gsb.primary
+								textAlign: "center",
+								mt: "10vh",
+								color: "#fff"
 							}}
 						>
-							<Typography variant="body2">View Blogs</Typography>
-						</Button>
-					</Typography>
-				</Grid>
+							Bloggly
+						</Typography>
+						<Typography
+							variant="h4"
+							sx={{
+								textAlign: "center",
+								mt: "1vh",
+								mb: "calc(77vh - 17vh - 8.6876rem - 16px)",
+								color: "#eee"
+							}}
+						>
+							Blogs and stuff
+						</Typography>
 
-				<Grid item xs={12} sx={{ mt: "1rem" }}>
-					<Grid
-						container
-						spacing={2}
-						sx={{
-							alignItems: "center",
-							justifyContent: "center"
-						}}
-					>
-						{sectionItems.map(item => (
-							<Grid
-								item
-								xs={12}
-								md={3.5}
-								minHeight={300}
-								key={item.id}
+						<Typography
+							variant="h6"
+							sx={{
+								textAlign: "center",
+								color: "#333"
+							}}
+						>
+							come for the blogs and stay for the stuff
+						</Typography>
+						<Typography
+							component="div"
+							sx={{
+								textAlign: "center"
+							}}
+						>
+							<Button
+								variant="contained"
+								aria-label="view posts"
+								data-testid="viewAllPostsButton"
+								component={LinkBehavior}
+								to="/posts"
+								startIcon={<MessageIcon />}
 								sx={{
-									p: "0.25rem 0.5rem",
-									m: "1rem 2vw",
-									textAlign: "center",
-									backgroundColor: theme.palette.gsb.background,
+									mt: "0.25rem",
+									ml: "0.75rem",
+									p: "6px 16px",
 									borderRadius: "12px",
-									maxWidth: "200px",
-									color:
-										theme.palette.mode === "light"
-											? theme.palette.hover.table
-											: theme.palette.active
+									borderColor: theme.palette.gsb.primary,
+									backgroundColor: theme.palette.gsb.primary
 								}}
 							>
-								{item.icon}
-								<Typography sx={{ pt: "1rem" }}>{item.sentence}</Typography>
-							</Grid>
-						))}
+								<Typography variant="body2">View Blogs</Typography>
+							</Button>
+						</Typography>
+					</Grid>
+
+					<Grid item xs={12} sx={{ mt: "1rem" }}>
+						<Grid
+							container
+							spacing={2}
+							sx={{
+								alignItems: "center",
+								justifyContent: "center"
+							}}
+						>
+							{sectionItems.map(item => (
+								<Grid
+									item
+									xs={12}
+									md={3.5}
+									minHeight={220}
+									key={item.id}
+									sx={{
+										p: "0.75rem",
+										m: "1rem",
+										textAlign: "center",
+										backgroundColor: theme.palette.gsb.background,
+										borderRadius: "12px",
+										color:
+											theme.palette.mode === "light"
+												? theme.palette.hover.table
+												: theme.palette.active
+									}}
+								>
+									{item.icon}
+									<Typography sx={{ pt: "1rem" }}>{item.sentence}</Typography>
+								</Grid>
+							))}
+						</Grid>
 					</Grid>
 				</Grid>
-			</Grid>
+			</Container>
 		</Box>
 	);
 };
