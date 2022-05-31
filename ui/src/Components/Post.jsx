49eae2abc3b1bac5aecdb6598d6f7a951cae11cc
David Karrick <the.white.wolf.is.1337@gmail.com>
2022-05-30 19:20:15 -0600

    Feat: Initial work on editing post

--- ui/src/Components/Post.jsx
+++ ui/src/Components/Post.jsx
@@ -11,17 +11,30 @@ import {
 	DialogActions,
 	DialogTitle,
 	DialogContentText,
-	Slide
+	Slide,
+	InputAdornment,
+	TextField,
+	Tab,
+	Tabs
 } from "@mui/material";
-import { styled } from "@mui/material/styles";
-
-import MarkdownPreview from "@uiw/react-markdown-preview";
-import { useLocation, useNavigate, NavLink } from "react-router-dom";
 import {
 	ChevronLeftRounded as ChevronLeftRoundedIcon,
 	Delete,
-	Edit as EditIcon
+	Edit as EditIcon,
+	Save as SaveIcon,
+	Title as TitleIcon,
+	Cancel as CancelIcon
 } from "@mui/icons-material";
+import { styled } from "@mui/material/styles";
+
+import MarkdownPreview from "@uiw/react-markdown-preview";
+import CodeMirror from "@uiw/react-codemirror";
+import { markdown, markdownLanguage } from "@codemirror/lang-markdown";
+import { languages } from "@codemirror/language-data";
+import { materialLight } from "@ddietr/codemirror-themes/theme/material-light";
+import { materialDark } from "@ddietr/codemirror-themes/theme/material-dark";
+
+import { useLocation, useNavigate, NavLink } from "react-router-dom";
 
 import { PostAPI } from "#Api";
 import { UserContext } from "#Context";
@@ -34,6 +47,33 @@ const LinkBehavior = forwardRef((props, ref) => (
 const Transition = forwardRef(function Transition(props, ref) {
 	return <Slide direction="up" ref={ref} {...props} />;
 });
+
+const a11yProps = index => {
+	return {
+		id: `post-${index === 0 ? "edit" : "preview"}-tab`,
+		"aria-controls": `post-tabpanel-${index === 0 ? "edit" : "preview"}`
+	};
+};
+
+const TabPanel = props => {
+	const { children, value, index, ...other } = props;
+
+	return (
+		<div
+			role="tabpanel"
+			hidden={value !== index}
+			id={`post-tabpanel-${index === 0 ? "edit" : "preview"}`}
+			aria-labelledby={`post-${index === 0 ? "edit" : "preview"}-tab`}
+			{...other}
+		>
+			{value === index && (
+				<Box sx={{ p: 3 }}>
+					<Typography>{children}</Typography>
+				</Box>
+			)}
+		</div>
+	);
+};
 
 const Item = styled(Box)(({ theme }) => ({
 	...theme.typography.body2,
@@ -60,9 +100,24 @@ const DeleteDialog = ({ id, handleDelete, theme }) => {
 
 	return (
 		<div>
-			<Button onClick={handleClickOpen} sx={{ border: "none" }}>
-				<Delete sx={{ color: theme.palette.gsb.primary }} />
+			<Button
+				variant="contained"
+				aria-label="delete"
+				data-testid="deleteButton"
+				onClick={handleClickOpen}
+				sx={{
+					mt: "0.25rem",
+					ml: "0.75rem",
+					borderRadius: "12px",
+					color: theme.palette.gsb.primary,
+					backgroundColor: "transparent",
+					borderWidth: 0,
+					boxShadow: "none"
+				}}
+			>
+				<Delete />
 			</Button>
+
 			<Dialog
 				open={open}
 				TransitionComponent={Transition}
@@ -130,6 +185,17 @@ const Post = ({ id: postID, title, content, user, createdAt, updatedAt }) => {
 	const theme = useTheme();
 	const { user: User, setUser } = useContext(UserContext);
 	const [notif, setNotif] = useState({});
+	const [currentTab, setCurrentTab] = useState(0);
+	const [oldPostData, setOldPostData] = useState({
+		title,
+		content
+	});
+	const [newPostData, setNewPostData] = useState({
+		title,
+		content
+	});
+
+	const [editModeOn, setEditModeOn] = useState(false);
 	const isUserPost = location.pathname.startsWith("/user");
 	const { id: userID, firstName, lastName, username } = user;
 
@@ -146,6 +212,23 @@ const Post = ({ id: postID, title, content, user, createdAt, updatedAt }) => {
 
 	const notifClose = () => {
 		setNotif({});
+	};
+
+	const handleTitleChange = event => {
+		if (event.target.value.trim() === "") {
+			setNewPostData({ ...newPostData, title: oldPostData.title });
+		}
+		setNewPostData({ ...newPostData, title: event.target.value });
+	};
+	const handleMarkdownChange = value => {
+		if (value.trim() === "") {
+			setNewPostData({ ...newPostData, content: oldPostData.content });
+		}
+		setNewPostData({ ...newPostData, content: value });
+	};
+
+	const handleTabChange = (event, newValue) => {
+		setCurrentTab(newValue);
 	};
 
 	const handleDelete = value => {
@@ -185,6 +268,41 @@ const Post = ({ id: postID, title, content, user, createdAt, updatedAt }) => {
 			.catch(err => console.log(err));
 	};
 
+	const handleEdit = e => {
+		setEditModeOn(!editModeOn);
+	};
+
+	const handleSave = e => {
+		e.preventDefault();
+
+		const updatePost = new PostAPI();
+		updatePost
+			.id(postID)
+			.put(newPostData)
+			.then(response => {
+				if (response.ok) {
+					setNotif({
+						title: "Success",
+						message: "Post successfully updated",
+						severity: "success"
+					});
+					setOldPostData({ ...newPostData });
+					const refreshPosts = new PostAPI();
+					refreshPosts
+						.limit(0)
+						.userID(User.info.id)
+						.get()
+						.then(res => res.json())
+						.then(data => {
+							if (Array.isArray(data.data)) {
+								setUser({ ...User, posts: [...data.data] });
+							}
+						});
+				}
+			})
+			.catch(err => {});
+	};
+
 	return (
 		<MainContainer>
 			{Object.keys(notif).length > 0 && (
@@ -196,7 +314,6 @@ const Post = ({ id: postID, title, content, user, createdAt, updatedAt }) => {
 					afterClose={notifClose}
 				/>
 			)}
-
 			<Stack spacing={0}>
 				<Item>
 					<Button
@@ -229,38 +346,164 @@ const Post = ({ id: postID, title, content, user, createdAt, updatedAt }) => {
 					</Typography>
 				</Item>
 			</Stack>
-
 			<Box sx={{ p: "0.5rem" }}>
 				<Grid container spacing={2}>
-					<Grid item xs={10}>
-						<Typography variant="h3" sx={{ textAlign: "center" }}>
-							{title}
-						</Typography>
+					{editModeOn ? <Grid item xs={1}></Grid> : ""}
+
+					<Grid item xs={editModeOn ? 9 : 10}>
+						{editModeOn ? (
+							<TextField
+								id="title-input"
+								variant="standard"
+								value={newPostData.title}
+								label="Edit Title"
+								onChange={handleTitleChange}
+								InputProps={{
+									startAdornment: (
+										<InputAdornment position="start">
+											<TitleIcon
+												sx={{
+													color: theme.palette.gsb.primary,
+													fontSize: "2.75rem"
+												}}
+											/>
+										</InputAdornment>
+									)
+								}}
+								sx={{
+									"& .MuiInputBase-input": {
+										fontSize: "3rem",
+										lineHeight: 1.167,
+										color: theme.palette.text.primary,
+										textDecoration: "none"
+									},
+									"& label": {
+										mt: 1.5,
+										ml: 0,
+										color: theme.palette.primary.main
+									}
+								}}
+							/>
+						) : (
+							<Typography variant="h3" sx={{ textAlign: "center" }}>
+								{oldPostData.title}
+							</Typography>
+						)}
 					</Grid>
 
 					<Grid item xs={1}>
-						<Button sx={{ border: "none" }}>
-							<EditIcon sx={{ color: theme.palette.gsb.primary }} />
+						<Button
+							variant="contained"
+							aria-label={editModeOn ? "cancel" : "edit"}
+							data-testid={editModeOn ? "cancelButton" : "editButton"}
+							onClick={handleEdit}
+							sx={{
+								mt: "0.25rem",
+								ml: "0.75rem",
+								borderRadius: "12px",
+								color: theme.palette.gsb.primary,
+								backgroundColor: "transparent",
+								borderWidth: 0,
+								boxShadow: "none"
+							}}
+						>
+							{editModeOn ? <CancelIcon /> : <EditIcon />}
 						</Button>
 					</Grid>
 					<Grid item xs={1}>
-						<DeleteDialog
-							id={postID}
-							handleDelete={handleDelete}
-							theme={theme}
-						/>
+						{editModeOn ? (
+							<Button
+								variant="contained"
+								aria-label={"save"}
+								data-testid={"saveButton"}
+								startIcon={<SaveIcon />}
+								onClick={handleSave}
+								sx={{
+									mt: "0.25rem",
+									ml: "0.75rem",
+									borderRadius: "12px",
+									color: theme.palette.gsb.primary,
+									backgroundColor: "transparent",
+									borderWidth: 0,
+									boxShadow: "none"
+								}}
+							>
+								<Typography variant="body2">Save Changes</Typography>
+							</Button>
+						) : (
+							<DeleteDialog
+								id={postID}
+								handleDelete={handleDelete}
+								theme={theme}
+							/>
+						)}
 					</Grid>
 				</Grid>
 			</Box>
 
 			<Box sx={{ pl: "2rem", pr: "2rem" }}>
-				<MarkdownPreview
-					source={content}
-					style={{
-						color: theme.palette.text.primary,
-						backgroundColor: theme.palette.background.default
-					}}
-				/>
+				{editModeOn ? (
+					<>
+						<Box>
+							<Tabs
+								value={currentTab}
+								onChange={handleTabChange}
+								aria-label="post-tabs"
+							>
+								<Tab
+									label="Edit"
+									{...a11yProps(0)}
+									sx={{
+										backgroundColor:
+											currentTab === 0
+												? theme.palette.gsb.background
+												: "transparent"
+									}}
+								/>
+								<Tab
+									label="Preview"
+									{...a11yProps(1)}
+									sx={{
+										backgroundColor:
+											currentTab === 1
+												? theme.palette.gsb.background
+												: "transparent"
+									}}
+								/>
+							</Tabs>
+						</Box>
+						<TabPanel value={currentTab} index={0}>
+							<CodeMirror
+								value={newPostData.content}
+								height="100%"
+								theme={
+									theme.palette.mode === "light" ? materialLight : materialDark
+								}
+								extensions={[
+									markdown({ base: markdownLanguage, codeLanguages: languages })
+								]}
+								onChange={value => handleMarkdownChange(value)}
+							/>
+						</TabPanel>
+						<TabPanel value={currentTab} index={1}>
+							<MarkdownPreview
+								source={newPostData.content}
+								style={{
+									color: theme.palette.text.primary,
+									backgroundColor: theme.palette.background.default
+								}}
+							/>
+						</TabPanel>
+					</>
+				) : (
+					<MarkdownPreview
+						source={oldPostData.content}
+						style={{
+							color: theme.palette.text.primary,
+							backgroundColor: theme.palette.background.default
+						}}
+					/>
+				)}
 			</Box>
 		</MainContainer>
 	);
